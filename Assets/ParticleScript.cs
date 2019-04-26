e360cc95d9119689eb9934d6380d6eb85ee5e16d
homuhomuhyper <is0453hh@ed.ritsumei.ac.jp>
2019-04-26 16:09:42 +0900

    ビームのスクリプト作った

---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ParticleSystem ps_FX = GetComponent<ParticleSystem>();
+        if (Input.GetKey(KeyCode.Space)) {
+            ps_FX.Play();
+        }
+        
+    }
+}
+
